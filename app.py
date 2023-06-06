ef76103f2e6366974e9ca3baa61f282c893036ee
marcosvianaap <marcosap103@gmail.com>
2023-06-06 12:19:08 -0300

    config deploy

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
